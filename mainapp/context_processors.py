ec432d6eb15eddb75cf4ea3e937a0525807a731c
Grodlew <76842330+Grodlew@users.noreply.github.com>
2021-11-27 20:22:19 +0300

    Update context_processors.py

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -2,7 +2,7 @@ from basketapp.models import Basket
 
 
 def basket(request):
-    basket.items = []
+    basket_items = []
     if request.user.is_authenticated:
         basket_items = Basket.objects.filter(user=request.user)
     return {
