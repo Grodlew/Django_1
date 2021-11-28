da2244d59c1d48d930af797732be39c7dd792259
Grodlew <eskade02@gmail.com>
2021-11-28 16:55:33 +0300

    ДЗ 2.2

--- authapp/models.py
+++ authapp/models.py
@@ -4,11 +4,13 @@ import pytz
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True, verbose_name='Аватар')
-    age = models.PositiveSmallIntegerField(verbose_name='Возраст')
+    age = models.PositiveSmallIntegerField(verbose_name='Возраст', default=18)
 
     activate_key = models.CharField(max_length=128, verbose_name='Ключ активации', blank=True, null=True)
     activate_key_expired = models.DateTimeField(blank=True, null=True)
@@ -23,3 +25,29 @@ class ShopUser(AbstractUser):
         self.activate_key = None
         self.activate_key_expired = None
         self.save()
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+    OTHERS = 'O'
+
+    GENDERS = (
+        (MALE, 'Мужской'),
+        (FEMALE, 'Женский'),
+        (OTHERS, 'Пол не указан'),
+    )
+
+    user = models.OneToOneField(ShopUser, null=False, unique=True, on_delete=models.CASCADE, db_index=True)
+    tagline = models.CharField(max_length=128, verbose_name='Теги', blank=True)
+    about_me = models.TextField(verbose_name='Обо мне')
+    gender = models.CharField(choices=GENDERS, default=OTHERS, max_length=1, verbose_name='Пол')
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def update_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
